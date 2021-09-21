cf1eea2a75f9f2aa833ba32ec288d4c258a01260
Luc Patiny <luc@patiny.com>
2021-09-21 19:03:20 +0200

    feat: improve menu generation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,14 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
             // Please change this to your repo.
             editUrl:
               "https://github.com/hackuarium/docs.hackuarium.org/edit/main/website/",
+            sidebarItemsGenerator: async function ({
+              defaultSidebarItemsGenerator,
+              ...args
+            }) {
+              let sidebarItems = await defaultSidebarItemsGenerator(args);
+              sidebarItems = filterItems(sidebarItems);
+              return raisingSingleNodes(sidebarItems);
+            },
           },
           blog: {
             showReadingTime: true,
@@ -83,3 +91,31 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
       }),
   }
 );
+
+function filterItems(items) {
+  items = items.filter((item) => item.label !== "includes");
+  items.forEach((item) => {
+    if (Array.isArray(item.items)) {
+      item.items = filterItems(item.items);
+    }
+  });
+  return items;
+}
+
+function raisingSingleNodes(items) {
+  // we need to traverse the full hierarhy and if there is only one child items we raise it one level
+  for (let parentItem of items) {
+    if (parentItem && parentItem.items && parentItem.items.length) {
+      for (let j = 0; j < parentItem.items.length; j++) {
+        if (
+          parentItem.items[j].items &&
+          parentItem.items[j].items.length === 1
+        ) {
+          parentItem.items[j] = parentItem.items[j].items[0];
+        }
+      }
+      raisingSingleNodes(parentItem.items);
+    }
+  }
+  return items;
+}
