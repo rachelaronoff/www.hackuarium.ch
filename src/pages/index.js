adfc51ad4458472745f61cf91b7592fe0c629b14
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2024-05-22 15:28:00 +0200

    19:00 instead of 7pm

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
-        Meet us every Wednesday 7pm, @ <a href="https://maps.app.goo.gl/pMMcb69Lia883BiY8" target="__blank" style={{color: "var(--ifm-hero-text-color)"}}>Route de Crochy 20, 1024 Ecublens</a>
+        Meet us every Wednesday 19:00, @ <a href="https://maps.app.goo.gl/pMMcb69Lia883BiY8" target="__blank" style={{color: "var(--ifm-hero-text-color)"}}>Route de Crochy 20, 1024 Ecublens</a>
       </div>
     </header>
   )
